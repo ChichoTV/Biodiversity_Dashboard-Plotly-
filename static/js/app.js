22aa1eeb3c62ad3c2d499d716bcd179a5b103e9c
unknown <taylorvought@gmail.com>
2020-10-20 15:38:55 -0700

    more tus work

--- static/js/app.js
+++ static/js/app.js
@@ -14,6 +14,13 @@ var datab=d3.json('/../samples.json').then(data => {
       function filtering(samples){
         return samples.id==pat_id;
       }
+      var dem=data.metadata.filter(filtering);
+      var dem_dom=d3.select('#sample-metadata');
+      var list=dem_dom.selectAll('li');
+      list.data(Object.entries(dem[0]))
+        .text(function (d){
+          return d;
+        })
       var filtered=data.samples.filter(filtering);
       console.log(filtered);
       var tops=data.samples.map(sample=> sample.sample_values[0]);
@@ -22,16 +29,28 @@ var datab=d3.json('/../samples.json').then(data => {
       var otus=otu_ids.map(val=>'OTU '+val);
       Plotly.restyle('bar','x',[top_vals.reverse()]);
       Plotly.restyle('bar', 'y',[otus.reverse()]);
+      Plotly.relayout('bar',{title:`${pat_id}`});
 
 
     }
     selection.on('change',changer);
     console.log(pat_id);
+
     function init(){
       pat_id='940';
       function filtering(samples){
         return samples.id==pat_id;
       }
+      var dem=data.metadata.filter(filtering);
+      var dem_dom=d3.select('#sample-metadata');
+      dem_dom.append('ul');
+      var list=dem_dom.selectAll('li');
+      list.data(Object.entries(dem[0]))
+        .enter()
+        .append('li')
+        .text(function (d){
+          return d;
+        })
       var filtered=data.samples.filter(filtering);
       console.log(filtered);
       var tops=data.samples.map(sample=> sample.sample_values[0]);
@@ -45,44 +64,9 @@ var datab=d3.json('/../samples.json').then(data => {
         type: "bar",
         orientation: "h"
       };
-      var layout={title: selection};
+      var layout={title: pat_id};
       var to_plot=[trace1];
       Plotly.newPlot('bar',to_plot,layout);
     }
     init();
-    // function filtering(samples){
-    //   return samples.id==selection;
-    // }
-    // var filtered=data.samples.filter(filtering);
-    // var tops=data.samples.map(sample=> sample.sample_values[0]);
-    // // console.log(data.samples);
-    // // console.log(`-----------`);
-    // // console.log(tops);
-    // // console.log(`-----------`);
-    // // console.log(filtered);
-    // var top_vals=filtered['0'].sample_values.slice(0,10);
-    // var otu_ids=filtered['0'].otu_ids.slice(0,10);
-    // // console.log(otu_ids);
-    // var otus=otu_ids.map(val=>'OTU '+val);
-    // var trace1 = {
-    //     x: top_vals.reverse(),
-    //     y: otus.reverse(),
-    //     text: selection,
-    //     type: "bar",
-    //     orientation: "h"
-    //   };
-    // var layout={title: selection};
-    // var to_plot=[trace1];
-    // Plotly.newPlot('bar',to_plot,layout);
-
 })
-
-
-
-
-
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
